e2ba72dc73ace3b114212fbec65250379c73b7fb
Hector Matus <{ID}+{username}@users.noreply.github.com>
2024-02-27 22:15:39 -0600

    Validation of stock before to do payment and update stock

--- src/controllers/paymentsController.ts
+++ src/controllers/paymentsController.ts
@@ -1,3 +1,4 @@
+import e from "express";
 import { firestore } from "../firebase/config";
 import { PaymentI } from "../models/payment.model";
 const db = firestore;
@@ -6,6 +7,19 @@ export const makePaymentCinemaToDB = async (
   data: PaymentI
 ): Promise<{ error?: string; data?: PaymentI }> => {
   try {
+    const snapshot = await db
+      .collection("cinemas")
+      .doc(data.idCinema)
+      .collection("movies")
+      .doc(data.idMovie)
+      .get();
+    const snapshotData = snapshot.data();
+    if (!snapshotData) {
+      throw new Error("file doesn't exist");
+    }
+    if (snapshotData.stock <= 0) {
+      throw new Error("Movie doesn't have stock");
+    }
     const result = await db
       .collection("cinemas")
       .doc(data.idCinema)
@@ -18,6 +32,14 @@ export const makePaymentCinemaToDB = async (
       .doc(result.id)
       .update({
         id: result.id,
+      });
+    await db
+      .collection("cinemas")
+      .doc(data.idCinema)
+      .collection("movies")
+      .doc(data.idMovie)
+      .update({
+        stock: snapshotData.stock - 1,
       });
     return {
       data,
