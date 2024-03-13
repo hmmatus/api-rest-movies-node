5e4fdc2fae2de9a62aa69a41527fa176d18cddff
Hector Matus <{ID}+{username}@users.noreply.github.com>
2024-03-13 15:36:05 -0600

    Added admin register route, fixed getUserFor admin

--- src/modules/admins/adminDataAccess.ts
+++ src/modules/admins/adminDataAccess.ts
@@ -1,4 +1,5 @@
 import { auth, firestore } from "../../firebase";
+import { type UserResponseI } from "../users/userModel";
 import { type AdminI } from "./adminModel";
 
 export const registerAdminToDB = async (
@@ -23,6 +24,26 @@ export const registerAdminToDB = async (
       name: data.name,
       email: data.email,
       role: "admin",
+    };
+  } catch (error) {
+    throw new Error(`${(error as Error).message}`);
+  }
+};
+
+export const getAdminDataDB = async (
+  idUser: string,
+): Promise<UserResponseI> => {
+  try {
+    const result = await firestore.collection("admins").doc(idUser).get();
+    const data = result.data();
+    if (data == null) {
+      throw new Error("User doesn't exist");
+    }
+    return {
+      id: data.id,
+      name: data.name,
+      role: data.role,
+      email: data.email,
     };
   } catch (error) {
     throw new Error(`${(error as Error).message}`);
--- src/modules/admins/adminRoutes.ts
+++ src/modules/admins/adminRoutes.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import { API_VERSION } from "../../config";
 import adminController from "./adminController";
+import verifyToken from "../../middleware/isAuthenticated";
 const router = express.Router();
 
 router.post(`/${API_VERSION}/admins`, adminController.registerAdmin);
+router.get(
+  `/${API_VERSION}/admins/:id`,
+  verifyToken,
+  adminController.getUserData,
+);
 
 export default router;
